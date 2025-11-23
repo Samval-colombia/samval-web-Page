ccacc0af605e9ec088529f36a038c8f24cca965c
gilberto-galindo-samval <gilberto.galindo@samval.org>
2025-11-23 09:14:36 -0500

    base de inicio con  varios TODO

---
+++ src/app/layouts/header/components/navbar/navbar.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CONTACT_INFO, ContactInfo, NAVIGATION_MENU, NavigationItem, SOCIAL_MEDIA, SocialMedia } from './navigation.model';
+
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+
+  // SIgnals de datos
+  navigationItems = signal<NavigationItem[]>(NAVIGATION_MENU);
+  contactInfo = signal<ContactInfo>(CONTACT_INFO)
+  socialNetworks = signal<SocialMedia[]>(SOCIAL_MEDIA)
+
+
+  isMenuOpen = signal<boolean>(false);
+
+  toggleMenu(): void {
+    this.isMenuOpen.update(value => !value);
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen.set(false);
+  }
+
+}
---
+++ src/app/layouts/header/components/navbar/navigation.model.ts
@@ -0,0 +1,59 @@
+export interface NavigationItem{
+  label:string,
+  route : string
+}
+
+// Menu Navegacion
+
+export const NAVIGATION_MENU: NavigationItem[] =
+[
+  { label: 'Sobre nosotros', route: '/sobre-nosotros' },
+  { label: 'Contacto', route: '/contacto' },
+  { label: 'Productos', route: '/productos' },
+  { label: 'Proyectos', route: '/proyectos' },
+  { label: 'Etnoeducación', route: '/etnoeducacion' },
+  { label: 'Aliados', route: '/aliados' }
+
+]
+
+export interface ContactInfo {
+  phone: string;
+  email: string;
+  address: string;
+
+}
+
+export const CONTACT_INFO: ContactInfo = {
+  phone: '+57 3233207532',
+  email: 'fundacionsamval@gmail.com',
+  address: 'Colombia',
+
+};
+
+export interface SocialMedia {
+  name: string;
+  icon: string;
+  url: string;
+  active: boolean;
+}
+
+export const SOCIAL_MEDIA: SocialMedia[] = [
+  {
+    name: 'Facebook',
+    icon: 'fab fa-facebook-f',
+    url: '',
+    active: true
+  },
+  {
+    name: 'LinkedIn',
+    icon: 'fab fa-linkedin-in',
+    url: '',
+    active: true
+  },
+  {
+    name: 'Instagram',
+    icon: 'fab fa-instagram',
+    url: '',
+    active: true
+  }
+];
