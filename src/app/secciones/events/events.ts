fa504b49a2848b429ba2675cd8919a54701d2f48
gilberto-galindo-samval <gilberto.galindo@samval.org>
2025-11-24 11:20:51 -0500

    Update events.ts

--- src/app/secciones/events/events.ts
+++ src/app/secciones/events/events.ts
@@ -24,7 +24,7 @@ export class Events {
       image: '/assets/event-1.jpg',
       date: '01 Dic 2024',
       location: 'Barranquilla',
-      schedule: '8:00 AM - 10:00 AM',
+      schedule: '8:00 AM - 10:00',
       title: 'Arahuacos',
       description: `Los Arahuacos tienen una visión del mundo basada en la armonía con la naturaleza y el conocimiento ancestral. La enseñanza del pensamiento computacional mediante
       robots móviles se integrará con su cosmovisión sin imponer paradigmas occidentales y a su lengua materna.`
@@ -34,7 +34,7 @@ export class Events {
       image: '/assets/event-2.jpg',
       date: '01 Feb 2025',
       location: 'Medellín',
-      schedule: '8:00 AM - 10:00 AM',
+      schedule: '8:00 - 10:00',
       title: 'Formación Docente en Pensamiento Computacional',
       description: `Consejo comunitario Ma kankamana de san basilio de palenque, se fortalecerá la identidad cultural y el idioma (el palenquero) ya que se adaptará el lenguaje de programación a la lengua nativa la cual juegan un papel clave en la educación.
       Se diseñará la estrategias que respeten y fortalezcan su herencia cultural.`
