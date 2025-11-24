395b1cd3ac203bff130f5661f05c22ea42388930
gilberto-galindo-samval <gilberto.galindo@samval.org>
2025-11-24 11:17:03 -0500

    se añade la seccion de eventos
    aun falta implementar internacionalización en este y los demas componentes

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { About } from "../../secciones/about/about";
 import { Servicios } from "../../secciones/servicios/servicios";
+import { Events } from "../../secciones/events/events";
 
 @Component({
   selector: 'app-home',
-  imports: [About, Servicios],
+  imports: [About, Servicios, Events],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
---
+++ src/app/secciones/events/events.ts
@@ -0,0 +1,44 @@
+import { Component, signal } from '@angular/core';
+
+interface Event {
+  id: number;
+  image: string;
+  date: string;
+  location: string;
+  schedule : string,
+  title: string;
+  description: string;
+}
+
+@Component({
+  selector: 'app-events',
+  imports: [],
+  templateUrl: './events.html',
+  styleUrl: './events.css',
+})
+export class Events {
+
+  events = signal<Event[]>([
+    {
+      id: 1,
+      image: '/assets/event-1.jpg',
+      date: '01 Dic 2024',
+      location: 'Barranquilla',
+      schedule: '8:00 AM - 10:00 AM',
+      title: 'Arahuacos',
+      description: `Los Arahuacos tienen una visión del mundo basada en la armonía con la naturaleza y el conocimiento ancestral. La enseñanza del pensamiento computacional mediante
+      robots móviles se integrará con su cosmovisión sin imponer paradigmas occidentales y a su lengua materna.`
+    },
+    {
+      id: 2,
+      image: '/assets/event-2.jpg',
+      date: '01 Feb 2025',
+      location: 'Medellín',
+      schedule: '8:00 AM - 10:00 AM',
+      title: 'Formación Docente en Pensamiento Computacional',
+      description: `Consejo comunitario Ma kankamana de san basilio de palenque, se fortalecerá la identidad cultural y el idioma (el palenquero) ya que se adaptará el lenguaje de programación a la lengua nativa la cual juegan un papel clave en la educación.
+      Se diseñará la estrategias que respeten y fortalezcan su herencia cultural.`
+    }
+  ]);
+
+}
