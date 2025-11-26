9c411a7d867d838c358c97e4f1649e9cfcce2eaf
gilberto-galindo-samval <gilberto.galindo@samval.org>
2025-11-26 00:15:28 -0500

    modify translate service

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -1,13 +1,24 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, PLATFORM_ID, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Translation, TranslocoLoader } from '@ngneat/transloco';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
+  private platformId = inject(PLATFORM_ID);
 
   getTranslation(lang: string): Observable<Translation> {
+
+    if (!isPlatformBrowser(this.platformId)) {
+
+      return from(
+        import(`../assets/i18n/${lang}.json`).then(module => module.default)
+      );
+    }
+
+
     return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
   }
 }
